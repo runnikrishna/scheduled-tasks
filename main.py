d513bf9ddb35a6ba22a87a6045269a73c1a312d7
runnikrishna <u88.rahul@gmail.com>
2026-03-10 05:33:03 +0530

    Core logic of birthday wisher app
    The main.py file contains the core logic of the birthday wisher application.

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import datetime as dt
+import smtplib, random, pandas
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+OLD_CONTENT = '[NAME]'
+USERNAME = "r.mylearnspace@gmail.com"
+PASSWORD = "zfyhsszdqznrjkkx"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+current_date = dt.datetime.now()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+def update_wish(name):
+    files = os.listdir('./letter_templates')
+    current_file = random.choice(files)
+    file_path = os.path.join('./letter_templates', current_file)
+    with open(file_path, 'r') as file:
+        current_file_content = file.read()
+    updated_content = current_file_content.replace(OLD_CONTENT, name)
+    send_email(updated_content)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+def send_email(email_content):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(user=USERNAME, password=PASSWORD)
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            from_addr=USERNAME,
+            to_addrs="u88.rahul@gmail.com",
+            msg=f"Subject: Happy Birthday\n\n{email_content}"
         )
+
+birthday_dataset = pandas.read_csv('birthdays.csv')
+birthday_list = birthday_dataset.to_dict(orient="records")
+for record in birthday_list:
+    if current_date.month == record['month'] and current_date.day == record['day']:
+        update_wish(record['name'])
+
+
+
+
